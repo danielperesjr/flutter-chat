19234a08ba0bac04f33fdc213830be4a9370782b
Daniel Peres Jr <danielperes.junior@gmail.com>
2022-12-01 18:57:04 -0300

    style: implements Register screen UI

---
+++ lib/register.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+class Register extends StatefulWidget {
+  const Register({Key? key}) : super(key: key);
+
+  @override
+  State<Register> createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Cadastro"),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          color: Color(0xffe1f5fe),
+        ),
+        padding: EdgeInsets.all(16.0),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 42.0),
+                  child: Image.asset(
+                    "images/user.png",
+                    width: 200,
+                    height: 150,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 8.0),
+                  child: TextField(
+                    autofocus: true,
+                    keyboardType: TextInputType.text,
+                    style: TextStyle(fontSize: 20.0),
+                    decoration: InputDecoration(
+                      contentPadding:
+                      EdgeInsets.fromLTRB(32.0, 16.0, 32.0, 16.0),
+                      hintText: "Nome",
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(32),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 8.0),
+                  child: TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    style: TextStyle(fontSize: 20.0),
+                    decoration: InputDecoration(
+                      contentPadding:
+                          EdgeInsets.fromLTRB(32.0, 16.0, 32.0, 16.0),
+                      hintText: "E-mail",
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(32),
+                      ),
+                    ),
+                  ),
+                ),
+                TextField(
+                  keyboardType: TextInputType.text,
+                  style: TextStyle(fontSize: 20.0),
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.fromLTRB(32.0, 16.0, 32.0, 16.0),
+                    hintText: "Senha",
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(32),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 16.0, bottom: 10.0),
+                  child: MaterialButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Cadastrar",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
+                    ),
+                    color: Colors.lightBlue,
+                    padding: EdgeInsets.fromLTRB(32.0, 16.0, 32.0, 16.0),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(32)),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
