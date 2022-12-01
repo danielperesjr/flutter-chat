04541e1f482ebc589a8f51fcdc27093a9b38e770
Daniel Peres Jr <danielperes.junior@gmail.com>
2022-12-01 18:38:20 -0300

    style: implements Login screen UI

---
+++ lib/login.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          color: Color(0xffe1f5fe),
+        ),
+        padding: EdgeInsets.all(16.0),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                FlutterLogo(
+                  size: 145,
+                  style: FlutterLogoStyle.markOnly,
+                  curve: Curves.bounceInOut,
+                  duration: Duration(seconds: 5),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 16.0, bottom: 32.0),
+                      child: Text(
+                        "Flutter Chat",
+                        style: TextStyle(fontSize: 36, color: Colors.blueGrey),
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 8.0),
+                  child: TextField(
+                    autofocus: true,
+                    keyboardType: TextInputType.emailAddress,
+                    style: TextStyle(fontSize: 20.0),
+                    decoration: InputDecoration(
+                      contentPadding:
+                          EdgeInsets.fromLTRB(32.0, 16.0, 32.0, 16.0),
+                      hintText: "E-mail",
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(32),
+                      ),
+                    ),
+                  ),
+                ),
+                TextField(
+                  keyboardType: TextInputType.text,
+                  style: TextStyle(fontSize: 20.0),
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.fromLTRB(32.0, 16.0, 32.0, 16.0),
+                    hintText: "Senha",
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(32),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 16.0, bottom: 10.0),
+                  child: MaterialButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Entrar",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
+                    ),
+                    color: Colors.lightBlue,
+                    padding: EdgeInsets.fromLTRB(32.0, 16.0, 32.0, 16.0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(32)
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16.0),
+                  child: Center(
+                    child: GestureDetector(
+                      child: Text(
+                          "Ainda não possui conta? Cadastre-se!",
+                        style: TextStyle(
+                          color: Colors.blueGrey,
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      onTap: (){},
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
