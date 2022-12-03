c4e890d8ae9021280b87196363d53e1c0fa65368
Daniel Peres Jr <danielperes.junior@gmail.com>
2022-12-03 00:04:28 -0300

    feat: implements RouteGenerator class

---
+++ lib/route_generator.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chat/home.dart';
+import 'package:flutter_chat/login.dart';
+import 'package:flutter_chat/register.dart';
+
+class RouteGenerator {
+
+  static Route<dynamic>? generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case "/":
+        return MaterialPageRoute(
+          builder: (_) => Login(),
+        );
+      case "/login":
+        return MaterialPageRoute(
+          builder: (_) => Login(),
+        );
+      case "/register":
+        return MaterialPageRoute(
+          builder: (_) => Register(),
+        );
+      case "/home":
+        return MaterialPageRoute(
+          builder: (_) => Home(),
+        );
+      default:
+        _routeError();
+    }
+  }
+
+  static Route<dynamic> _routeError() {
+    return MaterialPageRoute(
+      builder: (_) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text("Tela não encontrada!"),
+          ),
+          body: Center(
+            child: Text("Tela não encontrada!"),
+          ),
+        );
+      },
+    );
+  }
+}
